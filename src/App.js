25817da35913bd3ebda13332ef64f0a4189609ad
Adrian Galvan <jaggalvan@hotmail.com>
2023-08-01 22:53:54 -0600

    feat: adding images and routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+import "semantic-ui-css/semantic.min.css";
+import {Button, Icon} from "semantic-ui-react";
+import Home from "./pages/Home"; 
+import Series from "./pages/Series";
+import Comics from "./pages/Comics";
+import Error404 from "./pages/Error404";
+import Header from "./components/Header";
+
+
+import { BrowserRouter, Route, Routes, switch } {
+  case value:
+    
+    break;
+
+  default:
+    break;
+} } from "react-router-dom";
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+      <Header/>
+      <Routes>
+
+      
+          <Route path="/" exact componet={ => <Home/>}/>
+          <Route path="/inicio" exact componen={() => <Home/>}/>
+          <Route path="/series" exact componet ={() => <Series/>}/>
+          <Route path="/comics" exact componet ={() => <Comics/>}/>
+          <Route path="*" element={<Error404/>}/>
+          <Route componet={() => <Error404/>}/>
+      </Routes>
+     
+      
+      </BrowserRouter>
+
+
     </div>
+    
   );
 }
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { Menu } from "semantic-ui-react";
+import { useLocation, useNavigate } from "react-router";
+import "./Header.scss";
+
+export default function Header() {
+  const currentPath = useLocation();
+  const finalCurrentPath = currentPath.pathname.replace("/", "");
+
+  const [activeItem, setActiveItem] = useState(finalCurrentPath);
+  const navigate = useNavigate();
+
+  //const history = useHistory();
+
+  const handleItemClick = (e, { name }) => {
+    setActiveItem(name);
+    navigate(`/${name}`)
+   // history.push(name);
+  };
+
+  return (
+    <div className="header-menu">
+      <Menu secondary>
+        <Menu.Item
+          name="inicio"
+          active={activeItem === "inicio"}
+          onClick={handleItemClick}
+        />
+        <Menu.Item
+          name="series"
+          active={activeItem === "series"}
+          onClick={handleItemClick}
+        />
+        <Menu.Item
+          name="comics"
+          active={activeItem === "comics"}
+          onClick={handleItemClick}
+        />
+      </Menu>
+    </div>
+  );
+}
